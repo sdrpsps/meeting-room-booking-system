d5e4ea9b7d56848542567b4f51e456d2d4325302
sunny <sunny@bytespark.app>
2024-07-22 13:57:51 +0800

    feat(后端): 请求成功格式拦截器

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { FormatResponseInterceptor } from './common/interceptors/format-response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new FormatResponseInterceptor());
   await app.listen(process.env.NEST_SERVER_PORT || 3000);
 }
 bootstrap();
